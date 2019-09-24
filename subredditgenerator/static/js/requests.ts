e82464698b1853494b03dd52a51f3a4b9d1065d7
Roxie Gibson <me@roxxers.xyz>
2019-09-24 16:24:05 +0100

    Added form and eventlistener to take input for apirequests

--- subredditgenerator/static/js/requests.ts
+++ subredditgenerator/static/js/requests.ts
@@ -1,9 +1,12 @@
 
 
-function refresh_titles(amount:number) {
+function get_titles(amount:number) {
     var request = new XMLHttpRequest()
+    var subreddit: string = document.forms["sInput"]["sName"].value
 
-    request.open('GET', '/api/v1/subreddit/gaysoundsshitposts/markov?amount=1', true)
+    var url: string = "/api/v1/subreddit/" + subreddit + "/markov?amount=1"
+
+    request.open('GET', url, true)
     
     request.onload = function() {
         var title = document.getElementById('gen')
@@ -13,3 +16,16 @@ function refresh_titles(amount:number) {
     
     request.send()
 }
+
+
+// Wrapper function to prevent the default submit and run the get_titles function
+var request_titles = function(event) {
+    get_titles(1);
+    event.preventDefault();
+};
+
+// Get subreddit form
+var form = document.getElementById("subreddit_request_form");
+
+// attach event listener
+form.addEventListener("submit", request_titles, true);
