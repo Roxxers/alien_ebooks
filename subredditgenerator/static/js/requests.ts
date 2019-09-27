b1249fc6d954cc44caf57dade23d18d78587c76f
Roxie Gibson <me@roxxers.xyz>
2019-09-28 00:39:24 +0100

    Fix: ts now pasrses the new json format correctly
    There is also a little neat change. Now the button will disable while the function loads the data.

--- subredditgenerator/static/js/requests.ts
+++ subredditgenerator/static/js/requests.ts
@@ -1,26 +1,31 @@
 
 
-function get_titles(amount:number) {
-    var request = new XMLHttpRequest()
-    var subreddit: string = document.forms["sInput"]["sName"].value
+function get_titles(amount: number) {
+    var request = new XMLHttpRequest();
+    var subreddit: string = document.forms["sInput"]["sName"].value;
 
-    var url: string = "/api/v1/subreddit/" + subreddit + "/markov?amount=1"
+    var url: string = "/api/v1/subreddit/" + subreddit + "/markov?amount=" + amount;
 
-    request.open('GET', url, true)
+    request.open('GET', url, true);
     
     request.onload = function() {
-        var title = document.getElementById('gen')
-        var data = JSON.parse(this.response)
-        title.innerHTML = data
+        var title = document.getElementById('gen');
+        var data = JSON.parse(this.response);
+
+        title.innerHTML = data["data"];
     }
     
-    request.send()
+    request.send();
 }
 
 
 // Wrapper function to prevent the default submit and run the get_titles function
-var request_titles = function(event) {
+function request_titles(event: Event) {
+    // We let button have any type here because otherwise the complier will complain that HTMLElement doesn't have atr 'disabled'
+    let button: any = document.getElementById("SubredditSubmitButton");
+    button.disabled = true;
     get_titles(1);
+    button.disabled = false;
     event.preventDefault();
 };
 
