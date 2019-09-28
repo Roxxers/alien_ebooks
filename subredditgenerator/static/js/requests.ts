40f341723ca8d50a85e1cd6fb7ddc589f9f66d71
Roxie Gibson <me@roxxers.xyz>
2019-09-28 18:27:01 +0100

    Refactor: Moved some things around in the ts files

--- subredditgenerator/static/js/requests.ts
+++ subredditgenerator/static/js/requests.ts
@@ -1,36 +1,38 @@
 
 
 function get_titles(amount: number) {
-    var request = new XMLHttpRequest();
-    var subreddit: string = document.forms["sInput"]["sName"].value;
-
-    var url: string = "/api/v1/subreddits/" + subreddit + "/markov?amount=" + amount;
-
-    request.open('GET', url, true);
     
+    const subreddit: string = document.forms["sInput"]["sName"].value;
+    const url: string = "/api/v1/subreddits/" + subreddit + "/markov?amount=" + amount;
+
+    var request = new XMLHttpRequest();
+
     request.onload = function() {
-        var title = document.getElementById('gen');
-        var data = JSON.parse(this.response);
+        var data: JSON = JSON.parse(this.response);
+        var title: HTMLElement = document.getElementById('gen');
 
         title.innerHTML = data["data"];
     }
     
+    request.open('GET', url, true);
     request.send();
 }
 
 
 // Wrapper function to prevent the default submit and run the get_titles function
 function request_titles(event: Event) {
-    // We let button have any type here because otherwise the complier will complain that HTMLElement doesn't have atr 'disabled'
+    // We let button have any type here because otherwise the complier will complain that HTMLElement doesn't have 'disabled'
     let button: any = document.getElementById("SubredditSubmitButton");
     button.disabled = true;
     get_titles(1);
     button.disabled = false;
+
     event.preventDefault();
 };
 
 // Get subreddit form
-var form = document.getElementById("subreddit_request_form");
+var form : HTMLElement = document.getElementById("subreddit_request_form");
 
 // attach event listener
 form.addEventListener("submit", request_titles, true);
+
