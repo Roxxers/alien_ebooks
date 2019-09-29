32d5006aec6325b5489778989a59afb8c7c835cf
Roxie Gibson <me@roxxers.xyz>
2019-09-29 03:40:50 +0100

    Feat: Give endpoint to task progress

--- subredditgenerator/api.py
+++ subredditgenerator/api.py
@@ -32,21 +32,21 @@ class SubredditMarkovEndpoint(SubredditResource):
     def get(self, name):
         # Maybe allow multiple subreddits in future
         parser = reqparse.RequestParser()
-        parser.add_argument('amount', default=1, type=int, help="Amount of markov-chain titles to return.")
+        parser.add_argument("amount", default=1, type=int, help="Amount of markov-chain titles to return.")
 
         name = name.lower()
         args = parser.parse_args()
         subreddit = models.Subreddit.get(name=name)
 
-        if subreddit:
-            # Found subreddit in db, created sentences from a markov chain
-            gen = markov.MarkovGenerator(subreddit, cache)
-            sentences = gen.generate_sentences(args["amount"])
-            message = "Generated titles successfully"
-            return self.response(200, message=message, data=sentences)
-        else:
-            # If we can't find the subreddit in the db, return 404
+        if not subreddit:
+            # If we can"t find the subreddit in the db, return 404
             return self.default_404_response("Subreddit not found in our database")
+
+        # Found subreddit in db, created sentences from a markov chain
+        gen = markov.MarkovGenerator(subreddit, cache)
+        sentences = gen.generate_sentences(args["amount"])
+        message = "Generated titles successfully"
+        return self.response(200, message=message, data=sentences)
 
 
 class SubredditEndpoint(SubredditResource):
@@ -55,29 +55,32 @@ class SubredditEndpoint(SubredditResource):
         with db_session:
             subreddit = models.Subreddit.get(name=name)
 
-        if subreddit:
-            data = {
-                "name": subreddit.name,
-                "number_of_titles": len(subreddit.titles)
-            }
-            return self.response(200, data=data)
-        else:
+        if not subreddit:
             return self.default_404_response("Subreddit not found in our database")
+
+        data = {
+            "name": subreddit.name,
+            "number_of_titles": len(subreddit.titles)
+        }
+        return self.response(200, data=data)
 
     def post(self, name):
         name = name.lower()
+        # TODO: Add check to see if subreddit already exists
 
         try:
             subreddit = reddit.subreddit(name)
-            _ = subreddit.subscribers  # This line does nothing but try and bait the exception out so we can return a 404 if it is not a real subreddit
+            # This line does nothing but try and bait the exception out so we can return a 404 if it is not a real subreddit
+            _ = subreddit.subscribers
 
             task = celery.add_titles_to_db.delay(name)
-            # TODO: This needs to be finished, basically figure out an endpoint for progress
-            print(task.status)
-            return task.status, 202
+            data = {
+                "task_id": task.id
+            }
+            return self.response(202, message=f"Created build task successfully for subreddit {name}", data=data)
         except exceptions.PrawcoreException:
             # Subreddit is not found
-            return self.default_404_response("Subreddit doesn't exist")
+            return self.default_404_response("Subreddit doesn' exist")
 
 
 class AllSubredditsEndpoint(SubredditResource):
@@ -88,9 +91,38 @@ class AllSubredditsEndpoint(SubredditResource):
         return self.response(200, data=data)
 
 
-class SubredditBuildEndpoint(SubredditResource):
-    def get(self, name):
-        pass
+class BuildTaskEndpoint(SubredditResource):
+    def get(self, task_id):
+        # Get task from queue
+        task = celery.add_titles_to_db.AsyncResult(task_id)
+
+        if not task.info:  
+            # If task does not exist
+            return self.default_404_response("Could not find task with that ID")
+
+        if task.state == "PENDING":
+            # job did not start yet
+            data = {
+                "state": task.state,
+                "current": task.info.get("current"),
+                "total": task.info.get("total"),
+                "status": "Pending..."
+            }
+        elif task.state != "FAILURE":
+            data = {
+                "state": task.state,
+                "current": task.info.get("current"),
+                "total": task.info.get("total"),
+                "status": task.info.get("status", "")
+            }
+        else:
+            # something went wrong in the background job
+            data = {
+                "state": task.state,
+                "status": str(task.info),  # this is the exception raised
+            }
+            return self.response(500, message="Task has failed.", data=data)
+        return self.response(200, data=data)
 
 
 API_BASE = "/api/v1"
@@ -98,3 +130,4 @@ API_BASE = "/api/v1"
 api.add_resource(AllSubredditsEndpoint, f"{API_BASE}/subreddits/")
 api.add_resource(SubredditEndpoint, f"{API_BASE}/subreddits/<name>")
 api.add_resource(SubredditMarkovEndpoint, f"{API_BASE}/subreddits/<name>/markov")
+api.add_resource(BuildTaskEndpoint, f"{API_BASE}/tasks/<task_id>")
--- subredditgenerator/celery.py
+++ subredditgenerator/celery.py
@@ -1,6 +1,6 @@
 
 from celery import Celery
-from pony.orm import db_session
+from pony.orm import db_session, TransactionIntegrityError
 
 from subredditgenerator import config, models, reddit
 
@@ -8,12 +8,35 @@ from subredditgenerator import config, models, reddit
 celery = Celery('background_tasks', broker=f"{config.REDIS_URI}0", backend=f"{config.REDIS_URI}0")
 
 
-@celery.task
+@celery.task(bind=True)
 @db_session
-def add_titles_to_db(subreddit_name: str):
+def add_titles_to_db(self, subreddit_name: str):
     # TODO: Add debug code here
-    # TODO: Add code to be able to get back progress on this process.
+    self.update_state(state="STARTED", meta={'status': "Started requesting data..."})
     subreddit = reddit.subreddit(subreddit_name)
-    db_sub = models.Subreddit(id=int(subreddit.id, 36), name=subreddit.display_name.lower())
-    for submission in subreddit.top(limit=None):
-        models.Titles(id=int(submission.id, 36), title=submission.title, subreddit=db_sub)
+    try:
+        amount_of_posts = 1000 # This is around the limit given by the api, just an estimate
+        # Set amount of processed posts and update state of task for api
+        processed = 0
+        meta = {
+            "status": "Processing data",
+            "total": amount_of_posts,
+            "current": processed,
+            "finished": False
+        }
+        self.update_state(state="PROCESSING", meta=meta)
+
+        # Add subreddit to subreddit table to setup the one to many link
+        db_sub = models.Subreddit(id=int(subreddit.id, 36), name=subreddit.display_name.lower())
+        
+        # Iterate through all posts we can get and add them to the database, incrementating status and processed each time
+        for submission in subreddit.top(limit=None):
+            models.Titles(id=int(submission.id, 36), title=submission.title, subreddit=db_sub)
+            meta["current"] += 1
+            self.update_state(state="PROCESSING", meta=meta)
+        meta["finished"] = True
+        self.update_state(state="FINISHED", meta=meta)
+
+    except TransactionIntegrityError as e:
+        # TODO: Add logging error here
+        pass
