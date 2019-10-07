6623c4734abfef0dc1b78f2f7caebb6e4920ab4d
Roxie Gibson <me@roxxers.xyz>
2019-10-08 00:20:18 +0100

    Refactor: Created a object to store site data for easy reuse

--- subredditgenerator/config.py
+++ subredditgenerator/config.py
@@ -4,6 +4,10 @@ from os.path import dirname, join
 
 from dotenv import load_dotenv
 
+INFO = {
+    "app_name": "ebooks_alien"
+}
+
 # We load the env file for when the program is run from source rather than docker.
 env_file = ".env"
 dotenv_path = join(dirname(__file__), "../", env_file)
@@ -18,18 +22,12 @@ DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_DATABASE = os.getenv("POSTGRES_DATABASE")
 DB_PORT = os.getenv("POSTGRES_PORT")
 
-
 REDIS_HOST = "redis" if USING_DOCKER else os.getenv('REDIS_HOST')
 REDIS_PORT = os.getenv("REDIS_PORT")
 REDIS_URI = "redis://{}:{}/".format(REDIS_HOST, REDIS_PORT)
 
-
-MEMCACHED_HOST = "memcached" if USING_DOCKER else os.getenv("MEMCACHED_HOST")
-MEMCACHED_PORT = os.getenv("MEMCACHED_PORT")
-
-
 CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
 CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
-USER_AGENT = "ebooks_alien. Source Code by u/rainbowroxxers at https://github.com/roxxers/example"
+USER_AGENT = "{[app_name]}. Source Code by u/rainbowroxxers at https://github.com/roxxers/example".format(INFO)
 
 SCHEDULER_API_ENABLED = True
--- subredditgenerator/routes.py
+++ subredditgenerator/routes.py
@@ -7,4 +7,4 @@ from subredditgenerator import app
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", info=app.config["INFO"])
