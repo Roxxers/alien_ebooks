5b2ff5a11fb008761b2e53caedfece247c5e4b02
Roxie Gibson <me@roxxers.xyz>
2019-11-08 22:36:34 +0000

    Moved element functions to new file

---
+++ alien_ebooks/src/elements.ts
@@ -0,0 +1,68 @@
+
+import { MarkovPost } from "./endpoints";
+
+/**
+ * Creates element that fits with the bulma framework
+ * @param tag - html tag for element
+ * @param classes - list of space separated classes to add to the element
+ * @returns Bulma HTML Element
+ */
+export function createElementWithClasses(tag: string, classes: string): HTMLElement {
+    const element = document.createElement(tag);
+    const classList = classes.split(" ");
+    classList.forEach(cssClass => {
+        element.classList.add(cssClass);
+    });
+    return element;
+}
+
+/**
+ * Creates a Bulma Media element.
+ * This is used for generated posts.
+ * @returns Bulma Media element
+ */
+export function createMediaElement(): HTMLElement {
+    // Base Element
+    const article = createElementWithClasses("article", "media");
+    // Media-left, image placeholder
+    const mediaImage = createElementWithClasses("div", "media-left");
+    const image = createElementWithClasses("div", "placeholder");
+    image.innerHTML = `<i class="fas fa-quote-left placeholder-icon"></i>`;
+    mediaImage.appendChild(image);
+    // Media Content
+    const mediaContent = createElementWithClasses("div", "media-content");
+    const contentWrapper = createElementWithClasses("div", "content");
+    mediaContent.appendChild(contentWrapper);
+    // Add all elements to base element
+    article.appendChild(mediaImage);
+    article.appendChild(mediaContent);
+    return article;
+}
+
+/**
+ * Creates post elements from api results
+ * @param posts - markov posts from the api
+ * @returns list of media objects
+ */
+export function createTitleElements(posts: MarkovPost[]): HTMLElement[] {
+    const articles: HTMLElement[] = [];
+    posts.forEach(post => {
+        const article = createMediaElement();
+        const contentElement = document.createElement("p");
+
+        let title = `<strong>${post.title}</strong>`;
+        if (post.nsfw) {
+            title += ` <span class="nsfw">NSFW</span>`;
+        }
+
+        const postMetaLine = `<span class="has-text-grey is-size-7"><strong>r/${post.subreddit}</strong> by u/exampleuser`;
+        const commentsCounter = `<i class="fas fa-comment-alt"></i> ${post.comments} Comments</span>`;
+
+
+        contentElement.innerHTML = `${title}<br>${postMetaLine}<br>${commentsCounter}`;
+        const mediaContent = article.getElementsByClassName("media-content");
+        mediaContent[0].children[0].appendChild(contentElement);
+        articles.push(article);
+    });
+    return articles;
+}
--- alien_ebooks/src/index.ts
+++ alien_ebooks/src/index.ts
@@ -14,79 +14,15 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import { createTitleElements } from "./elements";
 import { MarkovPost, SubredditMarkovEndpoint } from "./endpoints";
+
 // tslint:disable-next-line: no-var-requires
 require("./scss/bulma.scss");
 
 
 const API_ROOT: string = "/api/v1";
 const SUBREDDIT_ROOT: string = `${API_ROOT}/subreddits`;
-
-/**
- * Creates element that fits with the bulma framework
- * @param tag - html tag for element
- * @param classes - list of space separated classes to add to the element
- * @returns Bulma HTML Element
- */
-function createBulmaElement(tag: string, classes: string): HTMLElement {
-    const element = document.createElement(tag);
-    const classList = classes.split(" ");
-    classList.forEach(cssClass => {
-        element.classList.add(cssClass);
-    });
-    return element;
-}
-
-/**
- * Creates a Bulma Media element.
- * This is used for generated posts.
- * @returns Bulma Media element
- */
-function createMediaElement(): HTMLElement {
-    // Base Element
-    const article = createBulmaElement("article", "media");
-    // Media-left, image placeholder
-    const mediaImage = createBulmaElement("div", "media-left");
-    const image = createBulmaElement("div", "placeholder");
-    image.innerHTML = `<i class="fas fa-quote-left placeholder-icon"></i>`;
-    mediaImage.appendChild(image);
-    // Media Content
-    const mediaContent = createBulmaElement("div", "media-content");
-    const contentWrapper = createBulmaElement("div", "content");
-    mediaContent.appendChild(contentWrapper);
-    // Add all elements to base element
-    article.appendChild(mediaImage);
-    article.appendChild(mediaContent);
-    return article;
-}
-
-/**
- * Creates post elements from api results
- * @param posts - markov posts from the api
- * @returns list of media objects
- */
-function createTitleElements(posts: MarkovPost[]): HTMLElement[] {
-    const articles: HTMLElement[] = [];
-    posts.forEach(post => {
-        const article = createMediaElement();
-        const contentElement = document.createElement("p");
-
-        let title = `<strong>${post.title}</strong>`;
-        if (post.nsfw) {
-            title += ` <span class="nsfw">NSFW</span>`;
-        }
-
-        const postMetaLine = `<span class="has-text-grey is-size-7"><strong>r/${post.subreddit}</strong> by u/exampleuser`;
-        const commentsCounter = `<i class="fas fa-comment-alt"></i> ${post.comments} Comments</span>`;
-
-
-        contentElement.innerHTML = `${title}<br>${postMetaLine}<br>${commentsCounter}`;
-        const mediaContent = article.getElementsByClassName("media-content");
-        mediaContent[0].children[0].appendChild(contentElement);
-        articles.push(article);
-    });
-    return articles;
-}
 
 
 /**
